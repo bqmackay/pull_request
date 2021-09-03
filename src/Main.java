cefbbc71f7d4c412a931752e5b109d1459f5a33a
Byron Mackay <bqmackay@gmail.com>
2021-09-03 14:25:02 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a simple addition problem.");
+        System.out.println("This is a simple arithmetic problem.");
 
         int result = 2 - 1;
 
